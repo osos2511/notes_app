19d2d493d5035f5e35642a12173028bc10e008b3
osos2511 <om329211@gmail.com>
2023-09-28 09:04:55 +0300

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:notes_application/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('ososos'),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
